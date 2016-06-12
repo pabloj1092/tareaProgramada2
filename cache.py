54531523ba8046778056cde294d234e956afae91
Esteban Vargas <estebanva911@gmail.com>
2016-06-12 04:15:23 -0600

    Logica lista, pero falta hacer debug y pruebas

--- cache.py
+++ cache.py
@@ -8,21 +8,17 @@ class Cache(object):
 		b_size = 16
 
 		if((c_size & (c_size -1)) != 0):
-	    	print "Invalid cache size, use a base 2 number"
-	    	sys.exit()
-
-    	if((b_size & (b_size -1)) != 0):
-	    	print "Invalid block size, use a base 2 number"
-	    	sys.exit()	
-
-	    b_num = c_size/b_size	
-	    cache = [[0 for x in range(b_num)] for x in range(0,2)]
-
+			print "Invalid cache size, use a base 2 number"
+			sys.exit()
+		if((b_size & (b_size -1)) != 0):
+			print "Invalid block size, use a base 2 number"
+			sys.exit()	
+		b_num = c_size/b_size	
+		cache = [[0 for x in range(b_num)] for x in range(0,2)]
 		def read_c(read_index):
 			data_cache = cache[read_index][0]
 			state_c = cache[read_index][1]
 			return data_cache,state_c
-
 		def write_c(read_index, tag, state_c):
 			cache[read_index][0] = tag
 			cache[read_index][1] = state_c
--- main.py
+++ main.py
@@ -16,17 +16,14 @@ L1 = Cache(8)
 L2 = Cache(8)
 L3 = Cache(64)
 
-offset = int(math.log(float(b_size),2))
+
 index = int(math.log(float(b_size),2))
-offsetL3 = int(math.log(float(b_sizeL3),2))
 indexL3 = int(math.log(float(b_sizeL3),2))
 
 miss_local_L1 = 0
 miss_local_L2 = 0
-miss_local_L3 = 0
 hit_local_L1 = 0
 hit_local_L2 = 0
-hit_local_L3 = 0
 hit = 0
 miss = 0
 mask = 0
@@ -42,10 +39,55 @@ number_line = 1
 for line in open('aligned.trace'):
 	data = line.split()
 	instruction = line.split(' ')[1]
+	tag = int(data[0], 16) #still not the tag
+	read_index = tag & mask 
+	for i in range (index):
+		tag = (tag / 2) #now we have only the tag in $tag
+	
+	tagL3 = int(data[0], 16) #still not the tag
+	read_indexL3 = tagL3 & maskL3     #extract the index
+	for i in range (indexL3): #delete the index from the read
+		tagL3 = (tagL3 / 2) #now we have only the tag in $tag
 	if(number_line%2 == 0):
 		if(instruction == L):
-			resultL1 = P1.read()
+			local_L2,glob,state = P2.read(read_index, tag, read_indexL3, tagL3, L2, L1, L3)
+			if(local_L2 == 'miss_local'):
+				miss_local_L2 = miss_local_L2 + 1
+			else:
+				hit_local_L2 = hit_local_L2 + 1
+			if(glob == 'miss_global'):
+				miss = miss + 1
+			else:
+				hit = hit + 1
+		else:
+			local_L2,glob,state = P2.write(read_index, tag, read_indexL3, tagL3, L2, L1, L3)
+			if(local_L2 == 'miss_local'):
+				miss_local_L2 = miss_local_L2 + 1
+			else:
+				hit_local_L2 = hit_local_L2 + 1
+			if(glob == 'miss_global'):
+				miss = miss + 1
+			else:
+				hit = hit + 1
 	else:
-		P2
-
+		if(instruction == L):
+			local_L1,glob,state = P1.read(read_index, tag, read_indexL3, tagL3, L1, L2, L3)
+			if(local_L1 == 'miss_local'):
+				miss_local_L1 = miss_local_L1 + 1
+			else:
+				hit_local_L1 = hit_local_L1 + 1
+			if(glob == 'miss_global'):
+				miss = miss + 1
+			else:
+				hit = hit + 1
+		else:
+			local_L1,glob,state = P1.write(read_index, tag, read_indexL3, tagL3, L1, L2, L3)
+			if(local_L1 == 'miss_local'):
+				miss_local_L1 = miss_local_L1 + 1
+			else:
+				hit_local_L1 = hit_local_L1 + 1
+			if(glob == 'miss_global'):
+				miss = miss + 1
+			else:
+				hit = hit + 1
 	number_line = number_line +1
