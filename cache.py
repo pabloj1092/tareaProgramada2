93a62c9b9b1432ab894095f05cf3b8d53f924a85
Pablo Cubero Campos <pabloj@gmail.com>
2016-06-13 08:20:06 -0600

    fixing references

--- cache.py
+++ cache.py
@@ -12,13 +12,14 @@ class Cache(object):
 			sys.exit()
 		if((b_size & (b_size -1)) != 0):
 			print "Invalid block size, use a base 2 number"
-			sys.exit()	
-		b_num = c_size/b_size	
-		cache = [[0 for x in range(b_num)] for x in range(0,2)]
-		def read_c(read_index):
-			data_cache = cache[read_index][0]
-			state_c = cache[read_index][1]
-			return data_cache,state_c
-		def write_c(read_index, tag, state_c):
-			cache[read_index][0] = tag
-			cache[read_index][1] = state_c
+			sys.exit()
+		b_num = c_size/b_size
+		self.cache = [[0 for x in range(b_num)] for x in range(0,2)]
+
+	def read_c(self, read_index):
+		data_cache = self.cache[read_index][0]
+		state_c = self.cache[read_index][1]
+		return data_cache,state_c
+	def write_c(self, read_index, tag, state_c):
+		self.cache[read_index][0] = tag
+		self.cache[read_index][1] = state_c
